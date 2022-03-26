d6066a79ebfdbe84dc39adfaea564af3936e8e30
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-26 11:03:21 -0700

    imported and implemented module to execute shell script upon receiving form data

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const methodOverride = require('method-override');
+const cp = require('child_process');
 const app = express();
 
 app.use(express.json());
@@ -16,9 +17,47 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/pages/index.html');
 });
 
+
+//The POST request below is handling the form data
+
+app.post('/', (req, res) => {
+
+  // TODO: I have an if statement here because I'm not yet sure why two request bodies are being sent to the server and need to debug this. The first one being sent is an empty object, followed by the object I am expecting.
+  
+  if(!req.body.scenario) {
+    console.log(null);
+  } else {
+
+    const resolvedPath = `${require('path').resolve('./src/test.sh')}`;
+
+    /* 
+      Docs I'm using in case I need to come back to this:
+        - https://medium.com/stackfame/how-to-run-shell-script-file-or-command-using-nodejs-b9f2455cb6b7
+        - https://nodejs.org/api/child_process.html
+        - https://nodejs.org/api/child_process.html#child_processexeccommand-options-callback
+    */
+    cp.exec(`sh ${resolvedPath}`, (err, stdout, stderr) => {
+      console.log(stdout);
+      if (err) { console.error(`error: ${err}`); }
+      if (stderr) { console.log(`stderr: ${stderr}`); }
+    });
+
+    res.status(200).send( { status: 'ok' } );
+
+    /*
+      ---TODO---
+  
+      Render a different HTML page after form submission so that the above successful response status is more user friendly
+    */
+
+  }
+
+});
+
 app.get('*', (req, res) => {
   res.status(404).send('Page Not Found');
 });
+
 
 const start = (port) => {
 
